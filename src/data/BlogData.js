114e385e9b1f8b45582f1f37110417da700b3c96
Bushra Aboubida <58691127+dev-bushra@users.noreply.github.com>
2022-06-06 08:20:23 +0400

    Add sources code

---
+++ src/data/BlogData.js
@@ -0,0 +1,52 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Title",
+        tags:["Photoshop","etc.."],
+        date:"1 May, 2018",
+        imgSrc:"https://www.magnifico.ae/source/img/portfolio/ui/falak-tybe-ui-full.png",
+        link:"https://www.magnifico.ae/pages/portfolio.html"
+    },
+    {
+        id:2,
+        name:"Title",
+        tags:["Photoshop","etc.."],
+        date:"1 May, 2018",
+        imgSrc:"https://www.magnifico.ae/source/img/portfolio/ui/re-do-ui-full.png",
+        link:"https://www.magnifico.ae/pages/portfolio.html"
+    },
+    {
+        id:3,
+        name:"Title",
+        tags:["Photoshop","etc.."],
+        date:"1 May, 2018",
+        imgSrc:"https://www.magnifico.ae/source/img/portfolio/ui/mk-portfoilo-ui-full.png",
+        link:"https://www.magnifico.ae/pages/portfolio.html"
+    },
+    {
+        id:4,
+        name:"Title",
+        tags:["Photoshop","etc.."],
+        date:"1 May, 2018",
+        imgSrc:"https://www.magnifico.ae/source/img/portfolio/ui/falak-tybe-ui-full.png",
+        link:"https://www.magnifico.ae/pages/portfolio.html"
+    },
+    {
+        id:5,
+        name:"Title",
+        tags:["Photoshop","etc.."],
+        date:"1 May, 2018",
+        imgSrc:"https://www.magnifico.ae/source/img/portfolio/ui/re-do-ui-full.png",
+        link:"https://www.magnifico.ae/pages/portfolio.html"
+    },
+    {
+        id:6,
+        name:"Title",
+        tags:["Photoshop","etc.."],
+        date:"1 May, 2018",
+        imgSrc:"https://www.magnifico.ae/source/img/portfolio/ui/mk-portfoilo-ui-full.png",
+        link:"https://www.magnifico.ae/pages/portfolio.html"
+    },
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,103 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        img: "https://www.magnifico.ae/source/img/favicon.png",
+        name:"Magnifico Agency Logo",
+        description:"Description text",
+        tags:["Photoshop","..etc"],
+        
+        demo:"https://"
+    },
+    {
+        id:2,
+        img: "https://www.magnifico.ae/source/img/favicon.png",
+        name:"Magnifico Agency Logo",
+        description:"Description text",
+        tags:["Photoshop","..etc"],
+        
+        demo:"https://"
+    },
+    {
+        id:3,
+        img: "https://www.magnifico.ae/source/img/favicon.png",
+        name:"Magnifico Agency Logo",
+        description:"Description text",
+        tags:["Photoshop","..etc"],
+        
+        demo:"https://"
+    },
+    {
+        id:4,
+        img: "https://www.magnifico.ae/source/img/favicon.png",
+        name:"Magnifico Agency Logo",
+        description:"Description text",
+        tags:["Photoshop","..etc"],
+        
+        demo:"https://"
+    },
+    {
+        id:5,
+        img: "https://www.magnifico.ae/source/img/favicon.png",
+        name:"Magnifico Agency Logo",
+        description:"Description text",
+        tags:["Photoshop","..etc"],
+        
+        demo:"https://"
+    },
+    {
+        id:6,
+        img: "https://www.magnifico.ae/source/img/favicon.png",
+        name:"Magnifico Agency Logo",
+        description:"Description text",
+        tags:["Photoshop","..etc"],
+        
+        demo:"https://"
+    },
+    {
+        id:7,
+        img: "https://www.magnifico.ae/source/img/favicon.png",
+        name:"Magnifico Agency Logo",
+        description:"Description text",
+        tags:["Photoshop","..etc"],
+        
+        demo:"https://"
+    },
+    {
+        id:8,
+        img: "https://www.magnifico.ae/source/img/favicon.png",
+        name:"Magnifico Agency Logo",
+        description:"Description text",
+        tags:["Photoshop","..etc"],
+        
+        demo:"https://"
+    },
+    {
+        id:9,
+        img: "https://www.magnifico.ae/source/img/favicon.png",
+        name:"Magnifico Agency Logo",
+        description:"Description text",
+        tags:["Photoshop","..etc"],
+        
+        demo:"https://"
+    },
+    {
+        id:10,
+        img: "https://www.magnifico.ae/source/img/favicon.png",
+        name:"Magnifico Agency Logo",
+        description:"Description text",
+        tags:["Photoshop","..etc"],
+        
+        demo:"https://"
+    },
+    {
+        id:11,
+        img: "https://www.magnifico.ae/source/img/favicon.png",
+        name:"Magnifico Agency Logo",
+        description:"Description text",
+        tags:["Photoshop","..etc"],
+        
+        demo:"https://"
+    }
+] 
